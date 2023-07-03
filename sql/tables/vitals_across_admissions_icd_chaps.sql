8ecb9ce9ac0441bdf9d60ea200ef64b472945dd6
TheaRoseBarnes <60779154+TheaRoseBarnes@users.noreply.github.com>
2023-07-03 14:20:55 +0100

    Update vitals_across_admissions_icd_chaps.sql

--- sql/tables/vitals_across_admissions_icd_chaps.sql
+++ sql/tables/vitals_across_admissions_icd_chaps.sql
@@ -1,10 +1,12 @@
 /* add another column to vitals_across_admissions_icd table which specifies
-  which broader disease chapter the icd code belongs to using icd_chapter_hierarchy.csv 
+  which broader disease chapter the icd code belongs to using icd_chapter_hierarchy.csv
   found in data /*
-  
--- Create vitals_across_admissions_icd_chaps
+
+/*-- Create vitals_across_admissions_icd_chaps
+
+ */
 CREATE TABLE vitals_across_admissions_icd_chaps AS
-SELECT *
-FROM vitals_across_admissions_icd
-JOIN icd_chapter_hierarchy
-ON icd_chapter_hierarchy.icd_code = vitals_across_admissions_icd.icd_code
+SELECT v.*, i.icd_chapter
+FROM vitals_across_admissions_icd v
+JOIN icd_chapter_hierarchy i
+ON i.icd_code = v.icd_code;
