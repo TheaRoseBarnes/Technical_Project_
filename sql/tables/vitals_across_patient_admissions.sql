1c24e9be09d51e457082309cfda2fda7c7bff34c
TheaRoseBarnes <60779154+TheaRoseBarnes@users.noreply.github.com>
2023-07-03 12:03:47 +0100

    Update vitals_across_patient_admissions.sql

--- sql/tables/vitals_across_patient_admissions.sql
+++ sql/tables/vitals_across_patient_admissions.sql
@@ -1,11 +1,15 @@
-/* uses tables chartevents and d_items from MIMIC-IV 2.0 database /*
+/* uses tables chartevents, d_items and patients from MIMIC-IV 2.0 database /*
 
--- Create vitals_across_patient_admissions
+/*-- Create vitals_across_patient_admissions
+
+ */
 CREATE TABLE vitals_across_patient_admissions AS
-SELECT v.subject_id, c.stay_id, c.itemid, c.hadm_id, c.charttime, c.storetime, c.value, c.valueuom
+SELECT v.subject_id, c.stay_id, c.itemid, c.hadm_id, c.charttime, c.storetime, c.value, c.valueuom,
+       p.gender, p.anchor_age
 FROM chartevents c
-LEFT JOIN patients_main v ON c.subject_id = v.subject_id
-LEFT JOIN d_items_main d ON c.itemid = d.itemid
+LEFT JOIN patients v ON c.subject_id = v.subject_id
+LEFT JOIN d_items d ON c.itemid = d.itemid
+LEFT JOIN patients p ON v.subject_id = p.subject_id
 WHERE c.itemid IN (220045, 220210, 220179, 220277, 223761, 226104, 223762, 226329, 220739, 223900, 223901)
   AND d.itemid IN (220045, 220210, 220179, 220277, 223761, 226104, 226329, 220739, 223900, 223901, 223762);
 
@@ -14,12 +18,14 @@ UPDATE vitals_across_patient_admissions
 SET itemid = (
     SELECT label
     FROM d_items
-    WHERE vitals_across_patient_admissions.itemid = d_items_main.itemid
+    WHERE vitals_across_patient_admissions.itemid = d_items.itemid
 );
 
 /* create refined table of vitals_across_patient_admissions for the eight vital measurements
 
--- Create refined_vitals_across_patient_admissions
+/* -- Create refined_vitals_across_patient_admissions
+
+ */
 CREATE TABLE refined_vitals_across_patient_admissions AS
 SELECT subject_id, stay_id, hadm_id, charttime, gender, anchor_age,
        CASE
