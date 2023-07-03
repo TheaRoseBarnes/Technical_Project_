c6c10ab0574f08bbf3e7a710eddaf7b7bc23a076
TheaRoseBarnes <60779154+TheaRoseBarnes@users.noreply.github.com>
2023-07-03 14:20:22 +0100

    Update vitals_across_admissions_icd.sql

--- sql/tables/vitals_across_admissions_icd.sql
+++ sql/tables/vitals_across_admissions_icd.sql
@@ -1,8 +1,11 @@
 /* use diagnoses_icd from MIMIC-IV 2.0 and refined_vitals_across_patient_admissions
   to produce a table that contains vitals across patient admissions and icd disease codes /*
 
--- Create vitals_across_admissions_icd
+/* -- Create vitals_across_admissions_icd
+
+ */
 CREATE TABLE vitals_across_admissions_icd AS
-SELECT *
-FROM refined_vitals_across_patient_admissions
-JOIN diagnoses_icd_main di ON refined_vitals_across_patient_admissions.hadm_id = di.hadm_id;
+SELECT v.*,
+       di.icd_code, di.icd_version
+FROM refined_vitals_across_patient_admissions v
+JOIN diagnoses_icd di ON v.hadm_id = di.hadm_id;
